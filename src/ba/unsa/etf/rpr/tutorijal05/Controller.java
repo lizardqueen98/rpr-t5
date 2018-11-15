eda1882013df0347381d7cc90f3b964cf94f7da9
lizardqueen98 <nadija.b@gmail.com>
2018-11-15 09:52:49 +0100

    digitron

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,57 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
 
+    public void btnDot(ActionEvent actionEvent) {
+    }
+
+    public void btnEquals(ActionEvent actionEvent) {
+    }
+
+    public void btnDiv(ActionEvent actionEvent) {
+    }
+
+    public void btnAdd(ActionEvent actionEvent) {
+    }
+
+    public void btnMod(ActionEvent actionEvent) {
+    }
+
+    public void btnMul(ActionEvent actionEvent) {
+    }
+
+    public void btnSub(ActionEvent actionEvent) {
+    }
+
+    public void btn7(ActionEvent actionEvent) {
+    }
+
+    public void btn8(ActionEvent actionEvent) {
+    }
+
+    public void btn6(ActionEvent actionEvent) {
+    }
+
+    public void btn9(ActionEvent actionEvent) {
+    }
+
+    public void btn4(ActionEvent actionEvent) {
+    }
+
+    public void btn5(ActionEvent actionEvent) {
+    }
+
+    public void btn1(ActionEvent actionEvent) {
+    }
+
+    public void btn2(ActionEvent actionEvent) {
+    }
+
+    public void btn3(ActionEvent actionEvent) {
+    }
+
+    public void btn0(ActionEvent actionEvent) {
+    }
 }
